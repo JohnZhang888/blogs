354234514721646ef7f9766ee9cbd30f72b5feb9
JohnZhang888 <zyk100859@163.com>
2026-06-02 13:46:52 +0800

    add pinValue & move the page datas to /page-data/

--- src/gen/article-content.js
+++ src/gen/article-content.js
@@ -1,18 +1,18 @@
 import { marked } from "https://cdn.osyb.cn/npm/marked/lib/marked.esm.js"
 
 export async function genArticleContent(pageID) {
-  const dataResponse = await fetch(`/${pageID}/data.json`);
-  const passageData = await dataResponse.json();
+  const dataResponse = await fetch(`/page-data/${pageID}/data.json`);
+  const pageData = await dataResponse.json();
 
-  const contentResponse = await fetch(`/${pageID}/content.md`);
+  const contentResponse = await fetch(`/page-data/${pageID}/content.md`);
   const passageMarkdown = await contentResponse.text();
 
   const markdownParsed = marked.parse(passageMarkdown);
 
   return `
-  <h1>${passageData.title}</h1>
-  <p>作者：${passageData.author}</p>
-  <p>日期：${passageData.date}</p>
+  <h1>${pageData.title}</h1>
+  <p>作者：${pageData.author}</p>
+  <p>日期：${pageData.date}</p>
   ${markdownParsed}
   `;
 }
